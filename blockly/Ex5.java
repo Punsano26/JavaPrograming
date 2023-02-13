0436d0f6fec5ff8f4fe776140aa32b06f0430747
Punsano26 <xlichaphlreuxn@gmail.com>
2023-02-13 16:51:12 +0700

    update 13/2/66

--- blockly/Ex5.java
+++ blockly/Ex5.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Ex5 {
     public static void main(String[] args){
         Scanner input = new Scanner (System.in);
-        int answer =1;
+        int answer =0;
         for (int i = 1;i <= 100; i++){
-            System.out.println(i);
-           
+           if (i %3 ==0){
+            answer= i;
+            System.out.println(answer);   
+            }
         }
-        do{
-            System.out.print();
-        }
+       
     }
 }
